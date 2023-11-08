91c5f623c8841bbef8cff8fc4e03732537fa26fb
joje3029 <joje3029@gmail.com>
2023-11-08 21:34:44 +0900

    스프링 부트 복습

---
+++ src/main/java/com/example/demo/controller/usrHomeController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class usrHomeController {
+	int a;
+	
+	usrHomeController(){
+		this.a=0;
+	}
+	
+	@RequestMapping("/usr/home/main")
+	@ResponseBody
+	public String showMain() {
+		return "뱁새씨";
+	}
+	
+	@RequestMapping("/usr/home/main2")
+	@ResponseBody
+	public int showMain2() {
+		return a++;
+	}
+}
