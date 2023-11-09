a444e0fdacaab87314189eb895dd4143f45b4247
joje3029 <joje3029@gmail.com>
2023-11-09 17:46:02 +0900

    수업때 DB 연결한거 시간 없어서 못한거 다함

--- src/main/java/com/example/demo/dao/ArticleDao.java
+++ src/main/java/com/example/demo/dao/ArticleDao.java
@@ -1,63 +1,21 @@
 package com.example.demo.dao;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.apache.ibatis.annotations.Mapper;
 
 import com.example.demo.vo.Article;
 
-@Component //@Dao 같은건 없음. @Component는 Controller나 Service처럼 이 역할만 꼭해야하는애들은 아니지만 스프링의 Bean 프로젝트에 올려서 스프링이 관리하게 할때 쓰는 애. 그래서 여기 @Component를 사용
-public class ArticleDao {
-	
-	private List<Article> articles;
-	private int lastArticleId;
-	
-	public ArticleDao() {
-		this.articles = new ArrayList<>();
-		this.lastArticleId = 0;
-		
-		makeTestData();
-	}
-	
-	public void makeTestData() {
-		for (int i = 1; i <= 10; i++) {
-			String title = "제목" + i;
-			String body = "내용" + i;
+@Mapper //Mapper는 class가 아니라 interface(100%추상클래스)가 되야함
+public interface ArticleDao {
 			
-			writeArticle(title, body);
-		}
-	}
+	public Article writeArticle(String title, String body);
 	
-	public Article writeArticle(String title, String body) {
-		this.lastArticleId++;
-		
-		Article article = new Article(lastArticleId, title, body);
-		
-		this.articles.add(article);
-		
-		return article;
-	}
+	public List<Article> getArticles();
 	
-	public List<Article> getArticles() {
-		return this.articles;
-	}
+	public Article getArticleById(int id);
 	
-	public Article getArticleById(int id) {
-		for (Article article : this.articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-		return null;
-	}
+	public void modifyArticle(Article article, String title, String body);
 	
-	public void modifyArticle(Article article, String title, String body) {
-		article.setTitle(title);
-		article.setBody(body);
-	}
-	
-	public void deleteArticle(Article article) {
-		this.articles.remove(article);
-	}
+	public void deleteArticle(Article article);
 }
