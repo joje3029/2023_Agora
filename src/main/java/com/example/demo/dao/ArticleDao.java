bf7d58e9d6d9051ab0e1348ccff6c03796d8c31f
joje3029 <joje3029@gmail.com>
2023-11-09 16:11:34 +0900

    수업 @Component에 대한 설명 추가함

--- src/main/java/com/example/demo/dao/ArticleDao.java
+++ src/main/java/com/example/demo/dao/ArticleDao.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 import com.example.demo.vo.Article;
 
-@Component
+@Component //@Dao 같은건 없음. @Component는 Controller나 Service처럼 이 역할만 꼭해야하는애들은 아니지만 스프링의 Bean 프로젝트에 올려서 스프링이 관리하게 할때 쓰는 애. 그래서 여기 @Component를 사용
 public class ArticleDao {
 	
 	private List<Article> articles;
