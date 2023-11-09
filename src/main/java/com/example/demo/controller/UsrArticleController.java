e4932a84d4e013acae3da350caf0a07bfe35e367
joje3029 <joje3029@gmail.com>
2023-11-09 13:44:36 +0900

    수업 테스트 데이터 만들기(현재 DB 안쓰고 articles 사용함) 스스로 한 버전

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.vo.Article;
 
-@Controller
+@Controller//어노테이션이 달림과 동시에 UsrArticleController 객체를 스프링이 만듦 : DI, IOC
 public class UsrArticleController {
 //	List<Article> artilces= new ArrayList<>(); 내가 한거. 틀린 이유 : 우리는 그냥 초기화를 바로 안하기로 했으니까. 나는 지금 그냥 바로 초기화를 하고 있으니까.
 	
@@ -25,9 +25,23 @@ public class UsrArticleController {
 	UsrArticleController(){
 		this.artilces=new ArrayList<>();
 		this.lastAritlceID=0;
+		
+		//테스트 데이터를 만들고 그 테스트 데이터를 생성자 안에서 호출 : 왜냐면 생성자는 생성이 됨과 동시에 만들어지니까. 그만 잊어먹어 지지배야!
+		TestData();
 	}
 	
-	@RequestMapping("/usr/ariicle/dowrite")
+	//테스트데이터 스스로 만듦. 잘했어! 성공도 했어! 오오!
+	private void TestData() {
+		int limitNum=10;
+		for(int i=1; i<=limitNum; i++) {
+			lastAritlceID++;
+			artilces.add(new Article(lastAritlceID, "제목"+i, "제목"+i));
+			
+		}
+		
+}
+
+	@RequestMapping("/usr/article/dowrite")
 	@ResponseBody
 	public String dowrite(String title, String body) { //그리고 dowrite 매소드가 파라미터(매개변수)를 보내야 아래의 Artilce이 받아서 쓰지 임마!! 다 아는거면서 다까묵었구만!!
 		lastAritlceID++; //왜냐면 Aritlce은 0번부터 시작하지 않으니까.
@@ -36,7 +50,7 @@ public class UsrArticleController {
 		return "뱁새씨";
 	}
 	
-	@RequestMapping("/usr/ariicle/showList")
+	@RequestMapping("/usr/article/showList")
 	@ResponseBody
 	public List<Article> showList() {
 		//dowrite에서 한거 보여주면 됨 => articles라는 ArrayList 보여줌.
