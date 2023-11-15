23075a1011f13e3cde009679193b541111753bdc
joje3029 <joje3029@gmail.com>
2023-11-15 20:28:58 +0900

    복습중. 기존의 controller가 화면 보여주던거에서 jsp로 보여주게 연결함. Util class의 f 함수가 하는 일인 String 포맷팅도 설명해놓음

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -13,15 +13,9 @@ public class UsrHomeController {
 		this.a=0;
 	}
 	
-	@RequestMapping("/usr/home/main")
-	@ResponseBody
+	@RequestMapping("/usr/home/main") //요청을 보내면 여기서 받아서
+//	@ResponseBody // ResponseBody는 controller에서 직접 화면을 그리려고 설정한것 그래서 jsp에게 화면을 그리게 할꺼면 넣으면 안됨.
 	public String showMain() {
-		return "뱁새씨";
-	}
-	
-	@RequestMapping("/usr/home/main2")
-	@ResponseBody
-	public int showMain2() {
-		return a++;
+		return "usr/home/main"; //jsp에게 리턴 => yml에서 설정한 prefix와 suffix가 붙어서 : /WEB-INF/jsp/usr/home/main.jsp로 찾아감.
 	}
 }
--- src/main/java/com/example/demo/util/Util.java
+++ src/main/java/com/example/demo/util/Util.java
@@ -10,8 +10,10 @@ public class Util {
 		return str.trim().length() == 0;
 	}
 
+	//여기저기서 형식 맞출때 쓰려고 public static
 	public static String f(String format, Object... args) {
 		return String.format(format, args);
+		// 형식을 맞추려고 String.format을 함.
 	}
 	
 }
