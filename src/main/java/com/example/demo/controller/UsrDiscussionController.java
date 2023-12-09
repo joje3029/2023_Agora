aa577ae867fba064917adadbdb6113c58aeee67f
joje3029 <joje3029@gmail.com>
2023-12-09 17:30:02 +0900

    2023.12.09 학원에서 작업 깃발. 밥먹고 집가서 채팅토론방 만들기

--- src/main/java/com/example/demo/controller/UsrDiscussionController.java
+++ src/main/java/com/example/demo/controller/UsrDiscussionController.java
@@ -1,13 +1,11 @@
 package com.example.demo.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.service.DiscussionService;
 import com.example.demo.util.Util;
